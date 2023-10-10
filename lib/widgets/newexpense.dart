237e00d1ed7e7f34983db1ac0c50d29425943df6
Ayush Talpelwar <ayushtalpelwar@gmail.com>
2023-10-10 23:01:08 +0530

    19

--- lib/widgets/newexpense.dart
+++ lib/widgets/newexpense.dart
@@ -1,3 +1,4 @@
+import 'package:expences_app/models/expense_model.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -10,12 +11,26 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final TitleController = TextEditingController();
   final amountController = TextEditingController();
-
+  DateTime? _selectedDate;
   @override
   void dispose() {
     TitleController.dispose();
     amountController.dispose();
     super.dispose();
+  }
+
+  void datepicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 
   @override
@@ -31,13 +46,39 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text("Title"),
             ),
           ),
-          TextField(
-            controller: amountController,
-            maxLength: 50,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              label: Text("Amount"),
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: "\$ ",
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_selectedDate == null
+                        ? "Select Date"
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                      onPressed: () {
+                        datepicker();
+                      },
+                      icon: Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
           Row(
             children: [
@@ -51,8 +92,10 @@ class _NewExpenseState extends State<NewExpense> {
               SizedBox(
                 width: 10,
               ),
-              ElevatedButton(
-                onPressed: () {},
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 child: Text("Cancel"),
               ),
             ],
